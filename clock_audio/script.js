fab31fdeb58bf4f2a7d49af6cebd96c747e9eefe
dvrklex <dvrklex@gmail.com>
2025-12-23 01:33:44 +0000

    clock_audio

---
+++ clock_audio/script.js
@@ -0,0 +1,60 @@
+const section = document.querySelector("section");
+const icons = document.querySelector(".icons");
+const bar = document.querySelector(".audio-fill");
+const threshold = document.querySelector(".audio-threshold");
+
+icons.onclick = () => section.classList.toggle("dark");
+
+// CLOCK
+setInterval(() => {
+  const d = new Date();
+  let h = d.getHours(), m = d.getMinutes(), s = d.getSeconds();
+  let ampm = h < 12 ? "AM" : "PM";
+
+  h = h % 12 || 12;
+  document.querySelector(".hour_num").innerText = String(h).padStart(2, "0");
+  document.querySelector(".min_num").innerText = String(m).padStart(2, "0");
+  document.querySelector(".sec_num").innerText = String(s).padStart(2, "0");
+  document.querySelector(".am_pm").innerText = ampm;
+}, 1000);
+
+// AUDIO
+function updateAudio(volume) {
+  const percent = Math.min(volume * 120, 100);
+  bar.style.width = percent + "%";
+  threshold.style.left = percent + "%";
+}
+
+if (window.wallpaperRegisterAudioListener) {
+  window.wallpaperRegisterAudioListener(data => {
+    const avg = data.reduce((a,b) => a + b) / data.length;
+    updateAudio(avg);
+  });
+} else {
+  // Mock CodePen
+  setInterval(() => {
+    updateAudio(Math.random());
+  }, 120);
+}
+const eqBars = document.querySelectorAll(".eq-bar");
+
+function updateEQ(volumeArray) {
+  eqBars.forEach((bar, i) => {
+    const v = volumeArray[i % volumeArray.length];
+    const height = Math.max(4, v * 80);
+    bar.style.height = height + "px";
+  });
+}
+
+if (window.wallpaperRegisterAudioListener) {
+  window.wallpaperRegisterAudioListener(data => {
+    updateEQ(data);
+  });
+} else {
+  // Mock para CodePen
+  setInterval(() => {
+    eqBars.forEach(bar => {
+      bar.style.height = (Math.random() * 80 + 4) + "px";
+    });
+  }, 120);
+}
